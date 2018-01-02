b6f3406a74141abc6c9dfea51a4b931627a29f98
durixx <32913495+durixx@users.noreply.github.com>
2018-01-03 00:55:35 +0100

    kostra gridu

---
+++ durix-ui.js
@@ -0,0 +1,46 @@
+$.fn.durixGrid = function (obj) {
+    var data = {
+        container: this
+    };
+
+    this.append("<div class=\"dx-container\">\n" +
+        "            <div class=\"dx-grid\">\n" +
+        "                <div class=\"dx-container-row-g1\">\n" +
+        "                    <div class=\"dx-grid-header dx-ds-color\"></div>\n" +
+        "                </div>\n" +
+        "                <div class=\"dx-container-row-g1\">\n" +
+        "                    <div class=\"dx-grid-body\">\n" +
+        "                        <div class=\"dx-container-row-g1\">\n" +
+        "                            <div class=\"dx-grid-column-header\"></div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"dx-container-row-g1\">\n" +
+        "                            <div class=\"dx-grid-column-body\"></div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "                <div class=\"dx-container-row-g1\">\n" +
+        "                    <div class=\"dx-grid-footer\">\n" +
+        "                        <div class=\"dx-column-basis-36px dx-justify-content-center dx-align-items-center\">\n" +
+        "                            <div class=\"dx-shapes-rectangle-1x10\"></div>\n" +
+        "                            <div class=\"dx-shapes-triangle-left-5px \"></div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"dx-column-basis-36px dx-justify-content-center dx-align-items-center\">\n" +
+        "                            <div class=\"dx-shapes-triangle-left-5px \"></div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"dx-column-basis-72px\"></div>\n" +
+        "                        <div class=\"dx-column-basis-36px dx-justify-content-center dx-align-items-center\">\n" +
+        "                            <div class=\"dx-shapes-triangle-right-5px \"></div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"dx-column-basis-36px dx-justify-content-center dx-align-items-center\">\n" +
+        "                            <div class=\"dx-shapes-triangle-right-5px \"></div>\n" +
+        "                            <div class=\"dx-shapes-rectangle-1x10\"></div>\n" +
+        "                        </div>\n" +
+        "                        <div class=\"dx-column-grow-x1\"></div>\n" +
+        "                        <div class=\"dx-column-basis-36px dx-justify-content-center dx-align-items-center\">\n" +
+        "                            <div class=\"dx-ico-refresh\"></div>\n" +
+        "                        </div>\n" +
+        "                    </div>\n" +
+        "                </div>\n" +
+        "            </div>\n" +
+        "        </div>");
+};
